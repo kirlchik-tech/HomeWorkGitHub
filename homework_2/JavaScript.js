cb061aaa98dffcb5464b1482ab3bdf3b60f42855
kirlchik-tech <kirsol20188@gmail.com>
2025-10-07 09:28:58 +0300

    Все задания

--- homework_2/JavaScript.js
+++ homework_2/JavaScript.js
@@ -1,3 +1,53 @@
+// Задание 1
+a = 20;
+alert(a);
+
+// Задание 2
+const firstIphoneYear = 2007;
+alert(firstIphoneYear);
+
+// Задание 3
+const jsCreator = "Брендан Эйх";
+alert(jsCreator);
+
+// Задание 4
+let num1 = 10;
+let num2 = 2;
+alert(num1 + num2);
+alert(num1 - num2);
+alert(num1 * num2);
+alert(num1 / num2);
+
+// Задание 5
+let result = 2 ** 5;
+alert(result);
+
+// Задание 6
+let a = 9;
+let b = 2;
+alert(a % b);
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+// Задание 8
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+// Задание 9
+const user = {
+    name: "Мария",
+    age: 25,
+    isAdmin: false
+};
+
 // Задание 10
 let userName = prompt("Как вас зовут?");
 alert(`Привет, ${userName}!`);
